494e0d93bc109266e3b04079ff5350b76708742d
Amitabhmalik <63450301+Amitabhmalik@users.noreply.github.com>
2020-09-30 18:11:44 +0530

    Update ReverseString.py

--- ReverseString.py
+++ ReverseString.py
@@ -1,19 +1,21 @@
-def reverseString(givenString):
-  j = len(givenString) - 1
-  index = 0
-
-  givenList = list(givenString)
-
-  while index < j:
-    temp = givenList[index]
-    givenList[index] = givenList[j]
-    givenList[j] = temp
-    index += 1
-    j -= 1
-
-  result = "".join(givenList)
-  return result
+inputString = input("What is your name? ")
+print("Your reversed name is ", inputString[::-1])
 
 
-inputString = (input("What is your name? "))
-print("Your reversed name is ", reverseString(inputString));
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
